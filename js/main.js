994d9311037d5086c4e6c6a9af4f2fdc600f9d70
rajaguru27 <jprsgurujprsguru@gmail.com>
2023-06-10 10:22:13 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// var mark=parseFloat(prompt("enter a value"));
+// if (mark>=35){
+// document.write("pass")
+// }
+// else{
+// document.write("fail");
+// }  
+
+// // To check whether Eleigible for voting
+
+// var age=parseFloat(prompt("enter the age"));
+// if(age>=18) 
+// {
+// document.write("Eleigible");
+// }
+// else
+// {
+// document.write("not Eleigible");
+// } 
+
+// // To  married and unmarried
+
+// var age=parseFloat(prompt("enter the age"));
+// if(age>=18)
+// {
+// document.write("married");
+// }
+// else
+// {
+// document.write("unmarried");
+// }
+
+// // job increment in  years
+
+// var years=parseFloat(prompt("enter the month"));
+// if (years>1000)
+// {
+// document.write("salary");
+// }
+// else
+// {
+// document.write("increment");
+// }
+
